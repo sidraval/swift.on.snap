beb67899938c5e4159d332e5b24ce0925f727adb
Sid Raval <sidsraval@gmail.com>
2015-01-30 18:00:05 -0500

    POST /api/users creates a user, no duplicate checking yet

--- backend/src/api/services/UserService.hs
+++ backend/src/api/services/UserService.hs
@@ -7,6 +7,7 @@ module Api.Services.UserService where
 import Control.Lens.TH
 import Control.Monad.State.Class
 import Data.ByteString.Char8 as B
+import Data.Maybe
 import Snap.Snaplet
 import Snap.Snaplet.PostgresqlSimple
 import Snap.Types
@@ -16,7 +17,16 @@ data UserService = UserService { _pg :: Snaplet Postgres }
 makeLenses ''UserService
 
 userRoutes :: [(B.ByteString, Handler b UserService ())]
-userRoutes = []
+userRoutes = [("/", method POST createUser)]
+
+createUser :: Handler b UserService ()
+createUser = do
+  deviceToken <- (getRequest >>= getDeviceToken)
+  newUser <- execute "INSERT INTO users (device_token) VALUES (?)" (Only deviceToken)
+  modifyResponse $ setResponseStatus 201 "Created"
+
+getDeviceToken :: (MonadSnap m) => Request -> m (Maybe ByteString)
+getDeviceToken rq = return $ getHeader "device-token" rq
 
 userServiceInit :: SnapletInit b UserService
 userServiceInit = makeSnaplet "userService" "Users service" Nothing $ do
