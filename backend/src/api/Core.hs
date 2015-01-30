1abf41fa1140e0a91d7c5a7ea7455b3333c5c2ae
Sid Raval <sidsraval@gmail.com>
2015-01-30 12:52:53 -0500

    initializers for Api, User snaplets

--- backend/src/api/Core.hs
+++ backend/src/api/Core.hs
@@ -1,8 +1,17 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Api.Core where
 
 import Api.Services.User
+import Control.Lens.TH
 import Snap.Snaplet
 
 data Api = Api { _user :: Snaplet User }
+
+makeLenses ''Api
+
+apiInit :: SnapletInit b Api
+apiInit = makeSnaplet "api" "Core Api" Nothing $ do
+  u <- nestSnaplet "user" user $ userInit
+  return $ Api u
