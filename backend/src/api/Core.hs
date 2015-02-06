c481832e7a6473321740f46c3a5343b914ee7dea
Sid Raval <sid@thoughtbot.com>
2015-02-06 13:08:59 -0500

    Nest attendance service snaplet into api

--- backend/src/api/Core.hs
+++ backend/src/api/Core.hs
@@ -5,11 +5,13 @@ module Api.Core where
 
 import Api.Services.UserService
 import Api.Services.EventService
+import Api.Services.AttendanceService
 import Control.Lens.TH
 import Snap.Snaplet
 
 data Api = Api { _userService  :: Snaplet UserService
                , _eventService :: Snaplet EventService
+               , _attendanceService :: Snaplet AttendanceService
                }
 
 makeLenses ''Api
@@ -18,4 +20,5 @@ apiInit :: SnapletInit b Api
 apiInit = makeSnaplet "api" "Core Api" Nothing $ do
   u <- nestSnaplet "users" userService userServiceInit
   e <- nestSnaplet "events" eventService eventServiceInit
-  return $ Api u e
+  a <- nestSnaplet "attendances" attendanceService attendanceServiceInit
+  return $ Api u e a
